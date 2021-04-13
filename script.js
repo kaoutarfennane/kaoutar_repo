5418305f63832d62783d40c55be2d606b5e3b66f
kaoutarfennane <44732551+kaoutarfennane@users.noreply.github.com>
2021-04-13 11:03:36 +0200

    premier_essai

---
+++ script.js
@@ -0,0 +1,11 @@
+/*var maVar ="ma valeur";
+(function(){
+    var maVar = "Valeur locale";
+    console.log(maVar);
+})();*/
+
+var tab=[1,2,3];
+for(let i=0;i<tab.length;i++)
+{
+    console.log(tab[i]);
+}
